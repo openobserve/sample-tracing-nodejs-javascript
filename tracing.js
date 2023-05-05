23c82285c9827775c550307588eab213eadd4b63
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-05-05 12:20:06 +0530

    refactor: updated file name to tracing.js

--- tracing.js
+++ tracing.js
@@ -1,9 +1,13 @@
 /*tracing.js*/
 // Require dependencies
 const opentelemetry = require("@opentelemetry/sdk-node");
-const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
-const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+const { diag, DiagConsoleLogger, DiagLogLevel } = require("@opentelemetry/api");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-http");
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
@@ -12,13 +16,13 @@ const sdk = new opentelemetry.NodeSDK({
   // traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
   traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
   traceExporter: new OTLPTraceExporter({
-    url: "https://otel.dev2.zinclabs.dev/v1/traces",
+    url: "https://alpha1.gke.zinclabs.dev/api/default/traces",
     headers: {
-      Authorization: "Basic YWRtaW46Q29tcGxleHBhc3MjMTIz",
+      Authorization: "Basic cm9vdEBleGFtcGxlLmNvbTpDb21wbGV4cGFzcyMxMjM=",
     },
   }),
   instrumentations: [getNodeAutoInstrumentations()],
   serviceName: "nodejs-javascript-service",
 });
 
-sdk.start()
+sdk.start();
